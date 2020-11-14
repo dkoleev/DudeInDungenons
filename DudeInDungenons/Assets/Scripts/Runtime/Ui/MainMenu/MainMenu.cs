f13de5fcf0d32dbaf20aa987016e162d90b3e5b8
DK <dmitry.koleev@game-garden.com>
2020-11-14 08:41:21 +0300

    add assets

--- DudeInDungenons/Assets/Scripts/Runtime/Ui/MainMenu/MainMenu.cs
+++ DudeInDungenons/Assets/Scripts/Runtime/Ui/MainMenu/MainMenu.cs
@@ -1,23 +1,65 @@
 using Runtime.Logic.GameProgress;
 using Sigtrap.Relays;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Runtime.Ui.MainMenu {
     public class MainMenu : UiBase {
-        [SerializeField]
+        private enum MenuCategory {
+            World,
+            Equipment
+        }
+
+        [SerializeField, Required]
+        private GameObject _worldCategory;
+        [SerializeField, Required]
+        private GameObject _equipmentCategory;
+        [SerializeField, Required]
+        private Button _worldButton;
+        [SerializeField, Required]
+        private Button _equipementButton;
+        [SerializeField, Required]
         private Inventory _inventory;
-        [SerializeField]
+        [SerializeField, Required]
         private Button _playButton;
+        
+        
         public Relay OnPlayClick = new Relay();
+
+        private MenuCategory _currentCategory = MenuCategory.Equipment;
 
         public override void Initialize(GameProgress progress, ItemsReference itemsReference) {
             base.Initialize(progress, itemsReference);
+            
+            SelectCategory(MenuCategory.World);
+            InitializeMainButtons();
             
             _inventory.Initialize(Progress, ItemsReference);
             _playButton.onClick.AddListener(() => {
                 OnPlayClick.Dispatch();
             });
+        }
+
+        private void InitializeMainButtons() {
+            _worldButton.onClick.AddListener(() => {
+                SelectCategory(MenuCategory.World);
+            });
+            
+            _equipementButton.onClick.AddListener(() => {
+                SelectCategory(MenuCategory.Equipment);
+            });
+        }
+
+        private void SelectCategory(MenuCategory category) {
+            if (_currentCategory == category) {
+                return;
+            }
+
+            _currentCategory = category;
+            
+            _worldCategory.SetActive(_currentCategory == MenuCategory.World);
+            _equipmentCategory.SetActive(_currentCategory == MenuCategory.Equipment);
         }
     }
 }
