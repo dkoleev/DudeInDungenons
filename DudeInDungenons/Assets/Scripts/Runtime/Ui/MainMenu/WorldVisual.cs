31f748b1e06e8f417cf45d97abf98deb76ed2c3d
DK <dmitry.koleev@game-garden.com>
2020-11-15 09:07:01 +0300

    add skyboxes

---
+++ DudeInDungenons/Assets/Scripts/Runtime/Ui/MainMenu/WorldVisual.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Runtime.Ui.MainMenu {
+    public class WorldVisual : MonoBehaviour {
+        [SerializeField]
+        private Transform _floatAnimationTarget;
+
+        [SerializeField]
+        private float _animationSpeed = 1.0f;
+        [SerializeField]
+        private float _floatingOffset = 10.0f;
+        
+        void Start() {
+            AnimateFloating();
+        }
+
+        private void AnimateFloating() {
+            var currentPosition = transform.localPosition;
+            if (_floatAnimationTarget == null) {
+                _floatAnimationTarget = transform;
+            }
+            
+            _floatAnimationTarget.DOMoveY(currentPosition.y + _floatingOffset, _animationSpeed).
+                SetLoops(-1, LoopType.Yoyo).
+                SetEase(Ease.InOutQuad);
+        }
+    }
+}
