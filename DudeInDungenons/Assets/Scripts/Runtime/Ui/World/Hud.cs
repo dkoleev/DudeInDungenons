5e7331489805ceeec66c021c39ccf2d9e9e1dd57
DK <dmitry.koleev@game-garden.com>
2020-11-18 22:40:49 +0300

    add game pause

--- DudeInDungenons/Assets/Scripts/Runtime/Ui/World/Hud.cs
+++ DudeInDungenons/Assets/Scripts/Runtime/Ui/World/Hud.cs
@@ -1,10 +1,24 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Runtime.Ui.World {
     public class Hud : MonoBehaviour {
+        [SerializeField, Required]
+        private Button _pauseButton;
+
+        [SerializeField, Required]
+        private PauseWindow _pauseWindow;
+        
         private OnScreenStickCustom _mover;
         private void Awake() {
             _mover = GetComponentInChildren<OnScreenStickCustom>();
+            
+            _pauseButton.onClick.AddListener(ShowPauseMenu);
+        }
+
+        private void ShowPauseMenu() {
+            _pauseWindow.Show();
         }
     }
 }
---
+++ DudeInDungenons/Assets/Scripts/Runtime/Ui/World/PauseWindow.cs
@@ -0,0 +1,28 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime.Ui.World {
+    public class PauseWindow : MonoBehaviour {
+        [SerializeField, Required]
+        private Button _resumeButton;
+
+        private void Awake() {
+            _resumeButton.onClick.AddListener(ResumeGame);
+        }
+
+        private void ResumeGame() {
+            Time.timeScale = 1.0f;
+            Hide();
+        }
+
+        public void Show() {
+            gameObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        private void Hide() {
+            gameObject.SetActive(false);
+        }
+    }
+}
