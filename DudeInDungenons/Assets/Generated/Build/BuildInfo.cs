2216ec4aa7e6b9d9ef2505299556a24e7a7568d2
DK <dmitry.koleev@game-garden.com>
2021-01-27 16:26:21 +0300

    add packages

---
+++ DudeInDungenons/Assets/Generated/Build/BuildInfo.cs
@@ -0,0 +1,40 @@
+namespace Generated.Build
+{
+    /// <summary>
+    ///     A collection of information providing further information as to the conditions present when the build was made.
+    /// </summary>
+    public static class BuildInfo
+    {
+        /// <summary>
+        ///     The builds numerically incremented version.
+        /// </summary>
+        /// <remarks>
+        ///     This may not be a shared build number across all build tasks.
+        /// </remarks>
+        public const int BuildNumber = 0;
+        /// <summary>
+        ///     The revision the workspace was at when the build was made.
+        /// </summary>
+        public const int Changelist = 0;
+        /// <summary>
+        ///     The specific build task used to create the build.
+        /// </summary>
+        public const string BuildTask = "N/A";
+        /// <summary>
+        ///     The version control stream which the build was built from.
+        /// </summary>
+        public const string Stream = "N/A";
+        /// <summary>
+        ///     The passed in build description.
+        /// </summary>
+        public const string Description = "N/A";
+        /// <summary>
+        ///     The internal description set through method invoke.
+        /// </summary>
+        public const string InternalDescription = "N/A";
+        /// <summary>
+        ///     The date and time when the build was started.
+        /// </summary>
+        public const string Timestamp = "N/A";
+	}
+}
